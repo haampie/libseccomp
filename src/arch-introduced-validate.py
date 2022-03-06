055a7644078267b368de68f32ba7f6c685b282b5
Tom Hromatka <tom.hromatka@oracle.com>
2022-03-05 19:56:00 -0700

    getting closer:
    The following is output:
    $ ./arch-introduced-validate.py
    Warning: arch x86_64 syscall accept has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    Warning: arch x86_64 syscall accept4 has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    Warning: arch x86_64 syscall access has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    Warning: arch x86_64 syscall acct has tag KV_PRE_5_17 but kernel has tag
    KV_5_10
    Warning: arch x86_64 syscall add_key has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    Warning: arch x86_64 syscall adjtimex has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    Warning: arch x86_64 syscall alarm has tag KV_PRE_5_17 but kernel has
    tag KV_5_10
    
    Signed-off-by: Tom Hromatka <tom.hromatka@oracle.com>

--- src/arch-introduced-validate.py
+++ src/arch-introduced-validate.py
@@ -3,16 +3,24 @@ import abc
 import os
 import subprocess
 
+FIRST_KNOWN_KERNEL = 5.04
 KERNEL_DICT = {
-        "KV_PRE_5_17": 0,
-        "KV_5_17": "v5.17",
-        "KV_5_18": "v5.18",
-
-        "KV_UNDEF": -1,
-        "KV_UNKNOWN": -2,
+        'v5.04': 'KV_5_04',
+        'v5.05': 'KV_5_05',
+        'v5.06': 'KV_5_06',
+        'v5.07': 'KV_5_07',
+        'v5.08': 'KV_5_08',
+        'v5.09': 'KV_5_09',
+        'v5.10': 'KV_5_10',
+        'v5.11': 'KV_5_11',
+        'v5.12': 'KV_5_12',
+        'v5.13': 'KV_5_13',
+        'v5.14': 'KV_5_14',
+        'v5.15': 'KV_5_15',
+        'v5.16': 'KV_5_16',
 }
 
-def run(command, shell=False):
+def run(command, shell=True):
     if shell:
         if isinstance(command, str):
             # nothing to do.  command is already formatted as a string
@@ -44,37 +52,68 @@ class Arch(object):
         # HACK
         self.kernel_path = '/home/thromatka/git/clean/upstream-torvalds'
         self.syscall_file = None
+        self.git_blame = None
+        self.arch_name = None
 
         # a dictionary of syscalls and the line number they appear on
         # e.g. self.syscall_dict['utime'] = 143
         self.syscall_dict = dict()
 
+    def run_git_blame(self):
+        cmd = 'pushd {} > /dev/null 2>&1;' \
+              'git blame {};' \
+              'popd'.format(self.kernel_path, self.syscall_file)
+
+        self.git_blame = run(cmd)
+
     @abc.abstractmethod
     def build_syscall_dict(self):
         raise NotImplementedError
 
-    def get_commit_tag(self, syscall_name):
-        syscall_dir = os.path.dirname(self.syscall_file)
-        syscall_file = os.path.basename(self.syscall_file)
-        cmd = 'pushd {} > /dev/null 2>&1;' \
-              'git blame {};' \
-              'popd'.format(syscall_dir, syscall_file)
-
-        res = run(cmd, shell=True)
-
-        for line in res.splitlines():
+    def get_commit_hash(self, syscall_name):
+        for line in self.git_blame.splitlines():
             line_num_str = '{})'.format(self.syscall_dict[syscall_name])
             if line.find(line_num_str) != -1 and \
                line.find(syscall_name) != -1:
                    return line.split()[0]
+
+    def find_oldest_tag(self, commit_hash):
+        cmd = 'pushd {} > /dev/null 2>&1;' \
+              'git tag --contains {};' \
+              'popd'.format(self.kernel_path, commit_hash)
+        res = run(cmd)
+
+        tags = res.splitlines()
+        tags.sort()
+        for tag in tags:
+            if tag.find('-rc') == -1:
+                # this is the oldest non-release-candidate tag.  return it
+                return tag
+
+        raise OSError('Failed to find any tags associated with commit {}' \
+                      .format(commit_hash))
 
     def validate(self, syscall_name, tag_enum):
         if tag_enum == 'KV_UNDEF':
             # This syscall doesn't exist on this architecture
             return
 
-        commit_tag = self.get_commit_tag(syscall_name)
-        print('syscall {} was added in commit {}'.format(syscall_name, commit_tag))
+        commit_hash = self.get_commit_hash(syscall_name)
+        tag = self.find_oldest_tag(commit_hash)
+
+        try:
+            kernel_enum = KERNEL_DICT[tag]
+        except KeyError as ke:
+            if float(tag[1:]) < FIRST_KNOWN_KERNEL:
+                kernel_enum = 'KV_PRE_5_17'
+
+        if kernel_enum != tag_enum:
+            print('Warning: arch {} syscall {} has tag {} but kernel has' \
+                  ' tag {}'.format(self.arch_name, syscall_name, tag_enum,
+                  kernel_enum))
+            return False
+
+        return True
 
 
 class Arch_x86_64(Arch):
@@ -82,6 +121,8 @@ class Arch_x86_64(Arch):
         super().__init__()
         self.syscall_file = os.path.join(self.kernel_path,
                 'arch/x86/entry/syscalls/syscall_64.tbl')
+        self.run_git_blame()
+        self.arch_name = 'x86_64'
 
     def build_syscall_dict(self):
         cmd = "cat {}/arch/x86/entry/syscalls/syscall_64.tbl" \
@@ -92,12 +133,11 @@ class Arch_x86_64(Arch):
               " | awk '{{ print $3,$2 }}'" \
               " | sort ".format(self.kernel_path)
         
-        res = run(cmd, shell=True)
+        res = run(cmd)
 
         for line in res.splitlines():
             syscall,line_num = line.split()
             self.syscall_dict[syscall] = line_num
-        print(self.syscall_dict)
 
 
 validators = [
@@ -136,13 +176,15 @@ def parse_introduced_data(line, line_num):
                 len(cols), len(validators)))
 
     for idx, col in enumerate(cols):
+        error_cnt = 0
         col = col.strip()
 
         if idx == 0:
             syscall_name = col
         else:
             if validators[idx] is not None:
-                validators[idx].validate(syscall_name, col)
+                if not validators[idx].validate(syscall_name, col):
+                    error_cnt += 1
 
 
 if __name__ == '__main__':
@@ -151,4 +193,6 @@ if __name__ == '__main__':
             if idx == 0:
                 parse_introduced_header(line)
             else:
-                parse_introduced_data(line, idx)
+                error_cnt = parse_introduced_data(line, idx)
+
+    sys.exit(error_cnt)
